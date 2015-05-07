07c007a0ed27e6568b4cf201cf32439a30622a07
Oleg Hahm <oleg@hobbykeller.org>
2015-05-07 20:23:32 +0200

    DIRTY: add manually scheduled cells to macpong

--- projects/common/03oos_macpong/03oos_macpong.c
+++ projects/common/03oos_macpong/03oos_macpong.c
@@ -40,49 +40,89 @@ int mote_main(void) {
    board_init();
    scheduler_init();
    openstack_init();
-   if (idmanager_getMyID(ADDR_64B)->addr_64b[7]==0xbb) {
-      idmanager_setIsDAGroot(TRUE);
+   if (idmanager_getMyID(ADDR_64B)->addr_64b[7]==0x62) {
+       idmanager_setIsDAGroot(TRUE);
+
+       open_addr_t     temp_neighbor;
+       memset(&temp_neighbor,0,sizeof(temp_neighbor));
+       temp_neighbor.type             = ADDR_16B;
+       temp_neighbor.addr_16b[0]      = 0x99;
+       temp_neighbor.addr_16b[1]      = 0x83;
+
+       schedule_addActiveSlot(SCHEDULE_MINIMAL_6TISCH_SLOTOFFSET + SCHEDULE_MINIMAL_6TISCH_ACTIVE_CELLS + 1,
+               CELLTYPE_TX,
+               FALSE,
+               SCHEDULE_MINIMAL_6TISCH_CHANNELOFFSET,
+               &temp_neighbor
+               );
+       schedule_addActiveSlot(SCHEDULE_MINIMAL_6TISCH_SLOTOFFSET + SCHEDULE_MINIMAL_6TISCH_ACTIVE_CELLS + 2,
+               CELLTYPE_RX,
+               FALSE,
+               SCHEDULE_MINIMAL_6TISCH_CHANNELOFFSET,
+               &temp_neighbor
+              );
    }
+   else if ((idmanager_getMyID(ADDR_64B)->addr_64b[6]==0x99) && (idmanager_getMyID(ADDR_64B)->addr_64b[7]==0x83)) {
+       open_addr_t     temp_neighbor;
+       memset(&temp_neighbor,0,sizeof(temp_neighbor));
+       temp_neighbor.type             = ADDR_16B;
+       temp_neighbor.addr_16b[0]      = 0x21;
+       temp_neighbor.addr_16b[1]      = 0x62;
+
+       schedule_addActiveSlot(SCHEDULE_MINIMAL_6TISCH_SLOTOFFSET + SCHEDULE_MINIMAL_6TISCH_ACTIVE_CELLS + 1,
+               CELLTYPE_RX,
+               FALSE,
+               SCHEDULE_MINIMAL_6TISCH_CHANNELOFFSET,
+               &temp_neighbor
+               );
+       schedule_addActiveSlot(SCHEDULE_MINIMAL_6TISCH_SLOTOFFSET + SCHEDULE_MINIMAL_6TISCH_ACTIVE_CELLS + 2,
+               CELLTYPE_TX,
+               FALSE,
+               SCHEDULE_MINIMAL_6TISCH_CHANNELOFFSET,
+               &temp_neighbor
+               );
+   }
+
    scheduler_start();
    return 0; // this line should never be reached
 }
 
 void macpong_initSend(opentimer_id_t id) {
-   if (idmanager_getIsDAGroot()==TRUE) {
-      return;
-   }
-   if (ieee154e_isSynch()==TRUE && neighbors_getNumNeighbors()==1) {
-      // send packet
-      //poipoimacpong_send(0);   
-      // cancel timer
-      opentimers_stop(macpong_vars.timerId);
-   }
+    if (idmanager_getIsDAGroot()==TRUE) {
+        return;
+    }
+    if (ieee154e_isSynch()==TRUE && neighbors_getNumNeighbors()==1) {
+        // send packet
+        //poipoimacpong_send(0);   
+        // cancel timer
+        opentimers_stop(macpong_vars.timerId);
+    }
 }
 
 void macpong_send(uint8_t payloadCtr) {
-   OpenQueueEntry_t* pkt;
-   uint8_t i;
-   
-   pkt = openqueue_getFreePacketBuffer(COMPONENT_UECHO);
-   if (pkt==NULL) {
-      openserial_printError(
-         COMPONENT_IPHC,
-         ERR_NO_FREE_PACKET_BUFFER,
-         (errorparameter_t)0,
-         (errorparameter_t)0
-      );
-      return;
-   }
-   pkt->creator                   = COMPONENT_IPHC;
-   pkt->owner                     = COMPONENT_IPHC;
-   
-   neighbors_getNeighbor(&pkt->l2_nextORpreviousHop,ADDR_64B,0);
-   packetfunctions_reserveHeaderSize(pkt,LEN_PAYLOAD);
-   ((uint8_t*)pkt->payload)[0]    = payloadCtr;
-   for (i=1;i<LEN_PAYLOAD;i++){
-     ((uint8_t*)pkt->payload)[i]  = i;
-   }
-   sixtop_send(pkt);
+    OpenQueueEntry_t* pkt;
+    uint8_t i;
+
+    pkt = openqueue_getFreePacketBuffer(COMPONENT_UECHO);
+    if (pkt==NULL) {
+        openserial_printError(
+                COMPONENT_IPHC,
+                ERR_NO_FREE_PACKET_BUFFER,
+                (errorparameter_t)0,
+                (errorparameter_t)0
+                );
+        return;
+    }
+    pkt->creator                   = COMPONENT_IPHC;
+    pkt->owner                     = COMPONENT_IPHC;
+
+    neighbors_getNeighbor(&pkt->l2_nextORpreviousHop,ADDR_16B,0);
+    packetfunctions_reserveHeaderSize(pkt,LEN_PAYLOAD);
+    ((uint8_t*)pkt->payload)[0]    = payloadCtr;
+    for (i=1;i<LEN_PAYLOAD;i++){
+        ((uint8_t*)pkt->payload)[i]  = i;
+    }
+    sixtop_send(pkt);
 }
 
 //=========================== stubbing ========================================
@@ -90,22 +130,22 @@ void macpong_send(uint8_t payloadCtr) {
 //===== IPHC
 
 void iphc_init(void) {
-   macpong_vars.timerId    = opentimers_start(
-      5000,
-      TIMER_PERIODIC,TIME_MS,
-      macpong_initSend
-   );
+    macpong_vars.timerId    = opentimers_start(
+            5000,
+            TIMER_PERIODIC,TIME_MS,
+            macpong_initSend
+            );
 }
 
 void iphc_sendDone(OpenQueueEntry_t* msg, owerror_t error) {
-   msg->owner = COMPONENT_IPHC;
-   openqueue_freePacketBuffer(msg);
+    msg->owner = COMPONENT_IPHC;
+    openqueue_freePacketBuffer(msg);
 }
 
 void iphc_receive(OpenQueueEntry_t* msg) {
-   msg->owner = COMPONENT_IPHC;
-   macpong_send(++msg->payload[0]);
-   openqueue_freePacketBuffer(msg);
+    msg->owner = COMPONENT_IPHC;
+    macpong_send(++msg->payload[0]);
+    openqueue_freePacketBuffer(msg);
 }
 
 //===== L3
@@ -151,8 +191,8 @@ void c6t_init(void)               { return; }
 void cinfo_init(void)             { return; }
 void cleds__init(void)            { return; }
 void cwellknown_init(void)        { return; }
-   // TCP
+// TCP
 void techo_init(void)             { return; }
-   // UDP
+// UDP
 void uecho_init(void)             { return; }
 
